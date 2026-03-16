48d8bc5433d76f183b2a2f292d2c320d41fc4803
DVD-BD <diegorivera3669@gmail.com>
2026-03-15 21:42:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,100 @@
 let carrito = 0;
 
 let productos = [
-{nombre:"Tenis Nike",precio:1200,img:"https://picsum.photos/200?1"},
-{nombre:"Sudadera Adidas",precio:850,img:"https://picsum.photos/200?2"},
-{nombre:"Playera Puma",precio:400,img:"https://picsum.photos/200?3"},
-{nombre:"Tenis Jordan",precio:2000,img:"https://picsum.photos/200?4"},
-{nombre:"Gorra Nike",precio:350,img:"https://picsum.photos/200?5"},
-{nombre:"Mochila Adidas",precio:900,img:"https://picsum.photos/200?6"},
-{nombre:"Pants Puma",precio:700,img:"https://picsum.photos/200?7"},
-{nombre:"Camisa Nike",precio:500,img:"https://picsum.photos/200?8"}
+
+{nombre:"Tenis Nike",precio:1200,categoria:"tenis",img:"https://picsum.photos/200?1",rating:5,oferta:true},
+
+{nombre:"Sudadera Adidas",precio:850,categoria:"ropa",img:"https://picsum.photos/200?2",rating:4},
+
+{nombre:"Playera Puma",precio:400,categoria:"ropa",img:"https://picsum.photos/200?3",rating:4},
+
+{nombre:"Tenis Jordan",precio:2000,categoria:"tenis",img:"https://picsum.photos/200?4",rating:5,oferta:true},
+
+{nombre:"Gorra Nike",precio:350,categoria:"accesorios",img:"https://picsum.photos/200?5",rating:3},
+
+{nombre:"Mochila Adidas",precio:900,categoria:"accesorios",img:"https://picsum.photos/200?6",rating:4}
+
 ];
 
-function cargarProductos(){
 
-let contenedor = document.getElementById("productos");
+function generarEstrellas(num){
 
-productos.forEach(p => {
+let estrellas="";
 
-contenedor.innerHTML += `
+for(let i=0;i<num;i++){
+
+estrellas+="★";
+
+}
+
+return estrellas;
+
+}
+
+
+function cargarProductos(lista){
+
+let contenedor=document.getElementById("productos");
+
+contenedor.innerHTML="";
+
+lista.forEach(p=>{
+
+contenedor.innerHTML+=`
+
 <div class="producto">
+
+${p.oferta ? '<div class="oferta">OFERTA</div>' : ''}
+
 <img src="${p.img}">
+
 <h3>${p.nombre}</h3>
+
+<div class="estrellas">${generarEstrellas(p.rating)}</div>
+
 <p class="precio">$${p.precio}</p>
+
 <button onclick="agregarCarrito()">Agregar</button>
+
 </div>
+
 `;
 
 });
 
 }
 
+
 function agregarCarrito(){
 
 carrito++;
 
-document.getElementById("contador").innerText = carrito;
+document.getElementById("contador").innerText=carrito;
 
 }
 
-cargarProductos();
 
+function mostrarCategoria(cat){
+
+if(cat=="todos"){
+
+cargarProductos(productos);
+
+return;
+
+}
+
+let filtrados=productos.filter(p=>p.categoria==cat);
+
+cargarProductos(filtrados);
+
+}
+
+
+cargarProductos(productos);
+
+
+/* SLIDER */
 
 let banners=[
 "https://picsum.photos/1200/300?1",
